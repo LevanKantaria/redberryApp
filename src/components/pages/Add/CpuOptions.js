e66e0a753c6c3a5d7bc738fe36d8d97d24f721d5
MrLeo7 <pillatok1@gmail.com>
2022-08-28 23:37:50 +0400

    added validation. added options for cpus

---
+++ src/components/pages/Add/CpuOptions.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+
+const CpuOptions = (props) => {
+  const [laptops, setLaptops] = useState([]);
+  let brandsGetUrl = "https://pcfy.redberryinternship.ge/api/cpus";
+  let optionList = [];
+
+  useEffect(() => {
+    let responseLaptops = [];
+    axios.get(brandsGetUrl).then((res) => {
+      let dataRes = res.data.data;
+
+      for (let key in dataRes) {
+        responseLaptops.push(dataRes[key]);
+      }
+
+      setLaptops(responseLaptops);
+    });
+  }, []);
+
+  for (let i = 0; i < laptops.length; i++) {
+    optionList.push(
+      <option key={Math.random()} id={laptops[i].id} value={laptops[i].id}>
+        {laptops[i].name}
+      </option>
+    );
+  }
+
+  return optionList;
+};
+
+export default CpuOptions;
--- src/components/pages/Add/PageTwo.js
+++ src/components/pages/Add/PageTwo.js
@@ -3,6 +3,8 @@ import { filterActions } from "../../../store";
 import classes from "./PageTwo.module.css";
 import BasicDragAndDrop from "./DragAndDrop2";
 import LaptopOptions from "./laptopOptions";
+import { useState } from "react";
+import CpuOptions from "./CpuOptions";
 
 const PageTwo = () => {
   const dispatch = useDispatch();
@@ -10,18 +12,150 @@ const PageTwo = () => {
     dispatch(filterActions.PageSelect("1"));
   };
 
+  const [laptopName, setLaptopName] = useState("");
+  const [laptopNameIsValid, setLaptopNameIsValid] = useState(false);
+  const [laptopNameIsTouched, setLaptopNameIsTouched] = useState(false);
+
+  const laptopNameChangeHandler = (event) => {
+    let value = event.target.value;
+    setLaptopNameIsTouched(true);
+    setLaptopName(value);
+    setLaptopNameIsValid(!/[.,<>?ა-ჰ]/.test(value));
+  };
+
+  const [cpuCore, setCpuCore] = useState("");
+  const [cpuCoreIsValid, setCpuCoreIsValid] = useState(false);
+  const [cpuCoreIsTouched, setCpuCoreIsTouched] = useState(false);
+
+  const cpuCoreChangeHandler = (event) => {
+    let value = event.target.value;
+    setCpuCoreIsTouched(true);
+    setCpuCore(value);
+    setCpuCoreIsValid(/^\d{1,3}$/.test(value));
+  };
+
+  const [cpuFlow, setCpuFlow] = useState("");
+  const [cpuFlowIsValid, setCpuFlowIsValid] = useState(false);
+  const [cpuFlowIsTouched, setCpuFlowIsTouched] = useState(false);
+
+  const cpuFlowChangeHandler = (event) => {
+    let value = event.target.value;
+    setCpuFlowIsTouched(true);
+    setCpuFlow(value);
+    setCpuFlowIsValid(/^\d{1,5}$/.test(value));
+  };
+
+  const [ram, setRam] = useState("");
+  const [ramIsValid, setRamIsValid] = useState(false);
+  const [ramIsTouched, setRamIsTouched] = useState(false);
+
+  const ramChangeHandler = (event) => {
+    let value = event.target.value;
+    setRamIsTouched(true);
+    setRam(value);
+    setRamIsValid(/^\d{1,3}$/.test(value));
+  };
+
+  const [drive, setDrive] = useState("");
+  const [driveIsValid, setDriveIsValid] = useState(false);
+  const [driveIsTouched, setDriveIsTouched] = useState(false);
+
+  const driveChangeHandler = (event) => {
+    setDrive(event.target.value);
+    setDriveIsTouched(true);
+    setDriveIsValid(true);
+  };
+
+  const [date, setDate] = useState("");
+  const dateChangeHandler = (event) => {
+    setDate(event.target.value);
+  };
+
+  const [price, setPrice] = useState("");
+  const [priceIsValid, setPriceIsValid] = useState(false);
+  const [priceIsTouched, setPriceIsTouched] = useState(false);
+  const priceChangeHandler = (event) => {
+    let value = event.target.value;
+    setPriceIsTouched(true);
+    setPriceIsValid(/^\d{1,4}$/.test(value));
+    setPrice(value);
+  };
+
+  const [condition, setCondition] = useState("");
+  const [conditionIsTouched, setConditionIsTouched] = useState(false);
+  const [conditionIsValid, setConditionIsValid] = useState(false);
+  const conditionChangeHandler = (event) => {
+    setCondition(event.target.value);
+    setConditionIsTouched(true);
+    setConditionIsValid(true);
+  };
+
+  let laptopNameInputClass = classes.nameInput;
+  if (!laptopNameIsValid && laptopNameIsTouched) {
+    laptopNameInputClass = classes.nameInputInvalid;
+  }
+
+  let cpuCoreClass = classes.nameInputNarrow;
+  if (!cpuCoreIsValid && cpuCoreIsTouched) {
+    cpuCoreClass = classes.nameInputNarrowInvalid;
+  }
+
+  let cpuFlowClass = classes.nameInputNarrow;
+  if (!cpuFlowIsValid && cpuFlowIsTouched) {
+    cpuFlowClass = classes.nameInputNarrowInvalid;
+  }
+
+  let ramClass = classes.nameInput;
+  if (!ramIsValid && ramIsTouched) {
+    ramClass = classes.nameInputInvalid;
+  }
+
+  let priceClass = classes.nameInput;
+  if (!priceIsValid && priceIsTouched) {
+    priceClass = classes.nameInputInvalid;
+  }
+
+  let conditionClass = classes.radioButtonsDiv;
+  if (!conditionIsValid && conditionIsTouched) {
+    conditionClass = classes.radioButtonsDivInvalid;
+  }
+
+  let conditionClassDrive = classes.radioButtonsDiv;
+  if (!driveIsValid && driveIsTouched) {
+    conditionClassDrive  = classes.radioButtonsDivInvalid;
+  }
+
+  
+
+  const saveHandler = (e) => {
+    e.preventDefault();
+    setLaptopNameIsTouched(true);
+    setCpuCoreIsTouched(true);
+    setCpuFlowIsTouched(true);
+    setRamIsTouched(true);
+    setDriveIsTouched(true)
+    setPriceIsTouched(true);
+    setConditionIsTouched(true);
+
+  };
   return (
     <div>
       <button className={classes.back} onClick={onBackHandler}>
         <img src="./Screenshot_4.jpg"></img>
       </button>
-      <form className={classes.form}>    
-               <BasicDragAndDrop />
+      <form className={classes.form}>
+        <BasicDragAndDrop />
         {/* Laptop Selectors */}
         <div className={classes.name_last_name}>
           <div className={classes.name}>
             <h5>ლეპტოპის სახელი</h5>
-            <input className={classes.nameInput} id="laptop" placeholder="HP" />
+            <input
+              className={laptopNameInputClass}
+              id="laptop"
+              placeholder="HP"
+              onChange={laptopNameChangeHandler}
+              value={laptopName}
+            />
             <h6> ლათინური ასოები, ციფრები, !@#$%^&*()_+= </h6>
           </div>
           <div className={classes.selectDiv}>
@@ -31,7 +165,7 @@ const PageTwo = () => {
               placeholder="ბრენდი"
             >
               <option>ლეპტოპის ბრენდი</option>
-              <LaptopOptions/>
+              <LaptopOptions />
             </select>
           </div>
         </div>
@@ -47,24 +181,26 @@ const PageTwo = () => {
               placeholder="cpu"
             >
               <option>CPU</option>
-              <option>sdfa</option>
+              <CpuOptions />
             </select>
           </div>
           <div className={classes.nameNarrow}>
             <h5>CPU-ს ბირთვი</h5>
             <input
-              className={classes.nameInputNarrow}
+              className={cpuCoreClass}
               id="core"
               placeholder="14"
+              onChange={cpuCoreChangeHandler}
             />
             <h6> მხოლოდ ციფრები </h6>
           </div>
           <div className={classes.name}>
             <h5>CPU-ს ნაკადი</h5>
             <input
-              className={classes.nameInputNarrow}
+              className={cpuFlowClass}
               id="hz"
               placeholder="365"
+              onChange={cpuFlowChangeHandler}
             />
             <h6> მხოლოდ ციფრები</h6>
           </div>
@@ -73,19 +209,36 @@ const PageTwo = () => {
         <div className={classes.name_last_name}>
           <div className={classes.name}>
             <h5>ლეპტოპის RAM (GB)</h5>
-            <input className={classes.nameInput} id="laptop" placeholder="HP" />
+            <input
+              className={ramClass}
+              id="laptop"
+              placeholder="GB"
+              onChange={ramChangeHandler}
+            />
             <h6> მხოლოდ ციფრები </h6>
           </div>
           {/* SSD */}
           <div className={classes.radioWrap}>
             <h5>მეხსიერების ტიპი</h5>
-            <div className={classes.radioButtonsDiv}>
+            <div className={conditionClassDrive}>
               <div className={classes.radioSelect}>
-                <input type="radio" id="drive1" value="ssd" name="drive" />
+                <input
+                  type="radio"
+                  id="drive1"
+                  value="ssd"
+                  name="drive"
+                  onChange={driveChangeHandler}
+                />
                 <label htmlFor="drive1">SSD</label>
               </div>
               <div className={classes.radioSelect}>
-                <input type="radio" id="drive2" value="hdd" name="drive" />
+                <input
+                  type="radio"
+                  id="drive2"
+                  value="hdd"
+                  name="drive"
+                  onChange={driveChangeHandler}
+                />
                 <label htmlFor="drive2">HDD</label>
               </div>
             </div>
@@ -99,16 +252,22 @@ const PageTwo = () => {
             <div className={classes.name}>
               <div className={classes.dateCorrection}>
                 <h5>შეძენის რიცხვი(არჩევითი)</h5>
-                <input type="date" className={classes.nameInput} id="date" />
+                <input
+                  type="date"
+                  className={classes.nameInput}
+                  id="date"
+                  onChange={dateChangeHandler}
+                />
               </div>
             </div>
             <div className={classes.selectDiv}>
               <div className={classes.name}>
                 <h5>ლეპტოპის ფასი</h5>
                 <input
-                  className={classes.nameInput}
+                  className={priceClass}
                   id="price"
                   placeholder="0000"
+                  onChange={priceChangeHandler}
                 />
                 <h6> მხოლოდ ციფრები</h6>
               </div>
@@ -120,13 +279,25 @@ const PageTwo = () => {
         <div className={classes.radioWrap}>
           <div className={classes.condition}>
             <h5>ლეპტოპის მდომარეობა</h5>
-            <div className={classes.radioButtonsDiv}>
+            <div className={conditionClass}>
               <div className={classes.radioSelect}>
-                <input type="radio" id="new" value="new" name="condition" />
+                <input
+                  type="radio"
+                  id="new"
+                  value="new"
+                  name="condition"
+                  onChange={conditionChangeHandler}
+                />
                 <label htmlFor="new">ახალი</label>
               </div>
               <div className={classes.radioSelect}>
-                <input type="radio" id="old" value="old" name="condition" />
+                <input
+                  type="radio"
+                  id="old"
+                  value="old"
+                  name="condition"
+                  onChange={conditionChangeHandler}
+                />
                 <label htmlFor="old">მეორადი</label>
               </div>
             </div>
@@ -134,10 +305,12 @@ const PageTwo = () => {
         </div>
         <div className={classes.formEndButtons}>
           <div>
-            <button className={classes.backButton}>უკან</button>
+            <button className={classes.backButton} onClick={onBackHandler} >უკან</button>
           </div>
           <div>
-            <button className={classes.saveButton}>დამახსოვრება</button>
+            <button className={classes.saveButton} onClick={saveHandler}>
+              დამახსოვრება
+            </button>
           </div>
         </div>
       </form>
