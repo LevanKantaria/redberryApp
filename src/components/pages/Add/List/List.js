1160d1fdc05482fb519becc88312ab18f7a7f211
MrLeo7 <pillatok1@gmail.com>
2022-08-31 15:07:44 +0400

    connected List Page to Api to dynamically display data

--- src/components/pages/Add/List/List.js
+++ src/components/pages/Add/List/List.js
@@ -1,24 +1,81 @@
 import "./List.css";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-
+import { useHistory } from "react-router-dom";
 const List = () => {
+  const history = useHistory();
+  const [laptops, setLaptops] = useState([]);
 
-    const [laptops, setLaptops] = useState([]);
-    let laptopApi = 'https://pcfy.redberryinternship.ge/api/laptops?token=681341299087a8ba7c5e905386371a21'
+  const onBackHandler = ()=> {
+    history.push('/welcome')
+  };
+  
+  let laptopApi =
+    "https://pcfy.redberryinternship.ge/api/laptops?token=681341299087a8ba7c5e905386371a21";
 
- axios.get(laptopApi).then(res=> console.log(res.data.data))
+  useEffect(() => {
+    axios.get(laptopApi).then((res) =>setLaptops(res.data.data));
+    
+  }, []);
+console.log(laptops)
+  let listItems = [];
+  listItems = laptops.map((item) => {
+    return(
+      <div className="laptop" key={Math.random()}>
+          <img
+            className="laptop__artwork"
+            src={'https://pcfy.redberryinternship.ge/' + item.laptop.image }
+          />
+          <div className="laptop__details">
+            <h2>{item.user.name} {item.user.surname}</h2>
+
+            <p className="laptop__artist">{item.laptop.name}</p>
+
+            <Link style={{ fontSize: "18px", color: "#4386A9" }} to="/welcome">
+              {" "}
+              მეტის ნახვა
+            </Link>
+          </div>
+        </div>
+    )
+  })
+// console.log(laptops.length)
+  // for(let key in laptops){
 
 
-  let listItems =[];
+  //   return(
+  //     <div className="laptop">
+  //         <img
+  //           className="laptop__artwork"
+  //           src="https://source.unsplash.com/random/312x312?v=1"
+  //         />
+  //         <div className="laptop__details">
+  //           <h2>{laptops[key]}</h2>
+
+  //           <p className="laptop__artist">Pentium lll</p>
+
+  //           <Link style={{ fontSize: "18px", color: "#4386A9" }} to="/welcome">
+  //             {" "}
+  //             მეტის ნახვა
+  //           </Link>
+  //         </div>
+  //       </div>
+  //   )
+  // }
 
   return (
     <div>
+      <div>
+        <button className="back" onClick={onBackHandler}>
+          <img src="./Screenshot_4.jpg"></img>
+        </button>
+      </div>
       <h3> ჩანაწერების სია</h3>
 
       <div className="laptops">
-        <div className="laptop">
+        {listItems }
+        {/* <div className="laptop">
           <img
             className="laptop__artwork"
             src="https://source.unsplash.com/random/312x312?v=1"
@@ -28,7 +85,10 @@ const List = () => {
 
             <p className="laptop__artist">Pentium lll</p>
 
-           <Link style={{fontSize:'18px' , color:'#4386A9'}} to='/welcome' > მეტის ნახვა</Link>
+            <Link style={{ fontSize: "18px", color: "#4386A9" }} to="/welcome">
+              {" "}
+              მეტის ნახვა
+            </Link>
           </div>
         </div>
         <div className="laptop">
@@ -39,7 +99,10 @@ const List = () => {
           <div className="laptop__details">
             <h2>laptop Title</h2>
             <p className="laptop__artist">Artist Name</p>
-            <p className="laptop__desc">short.</p>
+            <Link style={{ fontSize: "18px", color: "#4386A9" }} to="/welcome">
+              {" "}
+              მეტის ნახვა
+            </Link>
           </div>
         </div>
 
@@ -51,12 +114,12 @@ const List = () => {
           <div className="laptop__details">
             <h2>laptop Title</h2>
             <p className="laptop__artist">Artist Name</p>
-            <p className="laptop__desc">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum sed
-              sint doloremque repellat, iste debitis.
-            </p>
+            <Link style={{ fontSize: "18px", color: "#4386A9" }} to="/welcome">
+              {" "}
+              მეტის ნახვა
+            </Link>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
